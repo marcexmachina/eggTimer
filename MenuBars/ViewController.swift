e8cae139c3e0612018b8044dca4e03f304108cf3
Marc <marc.oneill2@gmail.com>
2016-11-23 22:08:47 +1100

    Egg timer app from Udemy complete iOS 10 developer course

--- MenuBars/ViewController.swift
+++ MenuBars/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer: Timer?
+    let defaultTime: String = "210"
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBAction func pausePressed(_ sender: Any) {
+        timer!.invalidate()
+    }
+   
+    
+    @IBAction func playPressed(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(processTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func minusTenPressed(_ sender: Any) {
+        let currentTime = Int(timerLabel.text!)!
+        if currentTime - 10 > 0 {
+            let newTime = currentTime - 10
+            timerLabel.text = String(newTime)
+        }
+        
+    }
+    
+    @IBAction func plusTenPressed(_ sender: Any) {
+        let currentTime = Int(timerLabel.text!)!
+        let newTime = currentTime + 10
+        timerLabel.text = String(newTime)
+        
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        timerLabel.text = defaultTime
+    }
+    
+    func processTimer() {
+        let currentTime  = Int(timerLabel.text!)!
+        if currentTime > 0 {
+            let newTime = currentTime - 1
+            timerLabel.text = String(newTime)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+                
     }
 
     override func didReceiveMemoryWarning() {
